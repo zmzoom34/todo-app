69fd1c87d0dfce01e743cbec9991bd29e2e83b6c
zm zoom <zmzoom34@gmail.com>
2025-01-31 20:50:08 +0300

    new update2

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,23 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-// import { getMessaging, onMessage, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAPjYOiuFetvMKExkehY6GtkimG-PsW3zU",
-    authDomain: "apartman-takip.firebaseapp.com",
-    databaseURL: "https://apartman-takip-default-rtdb.firebaseio.com",
-    projectId: "apartman-takip",
-    storageBucket: "apartman-takip.firebasestorage.app",
-    messagingSenderId: "1018320203975",
-    appId: "1:1018320203975:web:a320b649d0b7e2d60d4ee2",
-    measurementId: "G-NXR62T6MCV"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-// export const messaging = getMessaging(app);
-
-
